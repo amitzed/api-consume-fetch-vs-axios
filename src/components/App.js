e32b8bcd7a0ab36318e27dfa74871043ef8e29a7
amitzed <amitzmn@gmail.com>
2020-08-09 16:49:22 -0400

    All Routes working correctly and displaying data

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,15 @@ import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 import Header from './Header';
-import ListView from './ListView';
 import AxiosVersion from './AxiosVersion';
 import FetchVersion from './FetchVersion';
 
 const App = () => {
   return (
     <div>
-      <Header />
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={ListView} />
+          <Header />
           <Route path="/fetch" exact component={FetchVersion} />
           <Route path="/axios" exact component={AxiosVersion} />
         </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div>
-      Header
+      <Link to="/" style={{textDecoration: 'none'}}>
+        <h1>Home</h1>
+      </Link>
+      <Link to="/axios" style={{textDecoration: 'none'}}>
+        <h1>Axios Version</h1>
+      </Link>
+      <Link to="/fetch" style={{textDecoration: 'none'}}>
+        <h1>Fetch Version</h1>
+      </Link>
       <hr/>
     </div>
   )
