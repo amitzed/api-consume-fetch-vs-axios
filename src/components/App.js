84631496540c6b063b1cd2f6007ca0dd7d7eabb2
amitzed <amitzmn@gmail.com>
2020-08-09 15:51:41 -0400

    routes to /axios and /fetch are now going to their resprective pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
+import Header from './Header';
+import ListView from './ListView';
 import AxiosVersion from './AxiosVersion';
 import FetchVersion from './FetchVersion';
 
 const App = () => {
   return (
     <div>
-      <FetchVersion />
-      <AxiosVersion />
+      <Header />
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={ListView} />
+          <Route path="/fetch" exact component={FetchVersion} />
+          <Route path="/axios" exact component={AxiosVersion} />
+        </div>
+      </BrowserRouter>
     </div>
   )
 }
